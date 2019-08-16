c817ffd411b39a1a32e255e92b7d8e8e4a989127
dakkra <contact@dakkra.com>
2019-08-15 22:37:20 -0600

    Added manual open/close of file streams.
    List, open, and close commands added.
    
    This allows for testing to see if there are issues in software that scan directories with files that have an open stream handle to another software.

--- main.cpp
+++ main.cpp
@@ -4,9 +4,18 @@
 #include <chrono>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 //Determines if the threads should stay alive. When false, all threads should finish and close
 bool keepalive = true;
+std::unordered_map<std::string, std::ifstream *> open_files;
+
+std::string get_file_name() {
+    std::string input_buffer;
+    std::cout << "Enter a filename:";
+    std::cin >> input_buffer;
+    return input_buffer;
+}
 
 //Attempts to create a file with the provided filename
 void make_file(const std::string &file_name) {
@@ -50,9 +59,55 @@ void del_file(const std::string &file_name) {
     ifile.close();
 }
 
+//Attempts to open a file without closing the file stream and registers it with the open files map
+void open_file(const std::string &filename) {
+    std::cout << "Opening file " << filename << std::endl;
+    //Opens the file
+    auto ifile = new std::ifstream(filename);
+
+    //Make sure the stream is good
+    if (!ifile->good()) {
+        ifile->close();
+        delete (ifile);
+        std::cout << "File " << filename << " could not be opened! (maybe it doesn't exist?)" << std::endl;
+    }
+
+    //Register the file in the open file map
+    open_files[filename] = ifile;
+    std::cout << filename << " opened and registered" << std::endl;
+}
+
+//Closes the open stream to the desired registered file
+void close_file(const std::string &filename) {
+    std::cout << "Closing file " << filename << std::endl;
+
+    //Get the iterator position of the filename
+    auto it = open_files.find(filename);
+
+    //Check to see if the file is registered in the map
+    if (it == open_files.end()) {
+        std::cout << "File not registered with open file map" << std::endl;
+        return;
+    }
+
+    //Close the file stream and clear it from the map
+    open_files[filename]->close(); //Closes the stream
+    open_files.erase(it);//Removes the registry entry
+    std::cout << "File " << filename << " closed" << std::endl;
+}
+
+//Lists all currently open files
+void list_open_files() {
+    std::cout << "|>Listing open file streams:" << std::endl;
+    for (std::pair<std::string, std::ifstream *> pair : open_files) {
+        std::cout << "|-" << pair.first << std::endl;
+    }
+    std::cout << "^- End of list" << std::endl;
+}
+
 //Runs the interval spam, creating and deleting files with a timed delay
 void generate_interval(const size_t &number_of_intervals, const size_t &time_delay_ms, const std::string &filename) {
-    //Spit out data
+    //Spit out data about interval spam
     std::cout << "Running interval spam " << std::endl;
     std::cout << "Filename: " << filename << std::endl;
     std::cout << "Intervals: " << number_of_intervals << std::endl;
@@ -78,25 +133,26 @@ void process_command(const std::string &command) {
         std::cout << "make        -> creates a file" << std::endl;
         std::cout << "del         -> removes a file" << std::endl;
         std::cout << "time        -> runs interval file spam" << std::endl;
+        std::cout << "open        -> opens a file and does not close the stream" << std::endl;
+        std::cout << "close       -> closes the stream to an open file" << std::endl;
+        std::cout << "list        -> lists all currently opened files" << std::endl;
         std::cout << "exit        -> quits the application" << std::endl;
     } else if (command == "exit") {
         keepalive = false;
+    } else if (command == "open") {
+        open_file(get_file_name());
+    } else if (command == "close") {
+        close_file(get_file_name());
+    } else if (command == "list") {
+        list_open_files();
     } else if (command == "make") {
-        std::string file_name;
-        std::cout << "Enter a filename:";
-        std::cin >> file_name;
-        make_file(file_name);
+        make_file(get_file_name());
     } else if (command == "del") {
-        std::string file_name;
-        std::cout << "Enter a filename:";
-        std::cin >> file_name;
-        del_file(file_name);
+        del_file(get_file_name());
     } else if (command == "time") {
         size_t num_ints;
         size_t time_delay_ms;
-        std::string file_name;
-        std::cout << "Enter a filename:";
-        std::cin >> file_name;
+        std::string file_name = get_file_name();
         std::cout << "Enter number of interval iterations:";
         std::cin >> num_ints;
         std::cout << "Enter number ms between intervals:";
